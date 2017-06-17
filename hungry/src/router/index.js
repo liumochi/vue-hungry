18fa409b7e533826beb58a4086ec4cc5c6a39e0b
liumochi <m18846089770@163.com>
2017-06-17 17:32:51 +0800

    router

--- hungry/src/router/index.js
+++ hungry/src/router/index.js
@@ -4,6 +4,7 @@ import Homepage from '@/components/Homepage'
 import Login from '@/components/Login'
 import Myzone from '@/components/Myzone'
 import Order from '@/components/Order'
+import Business from '@/components/Business'
 
 Vue.use(Router)
 
